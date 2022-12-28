b0e8ed9b4522968fc8a7a5a4e8e198183ea88ed2
Admin Devops <admin@devops.com>
2022-12-28 10:30:26 +0700

    edit tests.py and added Jenkinsfile

--- polls/tests.py
+++ polls/tests.py
@@ -56,7 +56,7 @@ class QuestionIndexViewTests(TestCase):
         self.assertContains(response, "No polls are available.")
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
 
-     def test_past_question(self):
+    def test_past_question(self):
         """
         Questions with a pub_date in the past are displayed on the
         index page.
